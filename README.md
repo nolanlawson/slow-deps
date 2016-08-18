b861a80becba1fe06c0de78629e14ce340e66f25
Nolan Lawson <nolan.lawson@gmail.com>
2016-08-17 23:39:50 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+slow-deps
+====
+
+CLI to show which dependencies in a project are the slowest to `npm install`.
+
+Run this in a directory with a `package.json`, and it will analyze all the `dependencies`, 
+`devDependencies`, and `optionalDependencies`, then `npm install` each one in a temporary 
+directory with a temporary cache, then measure the install times, as well as the size on disk and the total number of transitive dependencies.
+
+Usage
+----
+
+    npm install -g slow-deps
+    slow-deps
+
+Example output
+----
+
+```
+Analyzing 7 dependencies...
+[===-----------------] 14% 0.0s
+[=========-----------] 43% 15.6s
+[==============------] 71% 15.8s
+[====================] 100% 0.0s
+--------------------------------------------------
+| Dependency           | Time  | Size   | # Deps |
+--------------------------------------------------
+| browserify           | 20.5s | 14 MB  | 94     |
+| promises-aplus-tests | 7.7s  | 2.4 MB | 24     |
+| jshint               | 6.9s  | 4.6 MB | 26     |
+| istanbul             | 6.2s  | 5.1 MB | 27     |
+| mocha                | 4.6s  | 924 KB | 16     |
+| uglify-js            | 3.9s  | 1.7 MB | 18     |
+| immediate            | 1.3s  | 24 KB  | 0      |
+--------------------------------------------------
+Total time: 51s
+Total size: 29 MB
+```
+
+Details
+----
+
+Note that this doesn't measure the cost of npm de-duplicating the sub-dependencies; 
+it only measures the time for installing each dependency independently.
+
+It's also worth noting that because this tool creates a temporary npm cache while it works,
+you'll be seeing the _uncached_ time for each package. Therefore your actual `npm install` should run a bit faster,
+assuming everythign is cached.
---
+++ index.js
@@ -0,0 +1,131 @@
+'use strict'
+
+if (!global.Promise) {
+  global.Promise = require('lie')
+}
+var temp = require('temp')
+var fs = require('fs')
+var exec = require('child-process-promise').exec
+var denodeify = require('denodeify')
+var shellEscape = require('shell-escape')
+var extend = require('js-extend').extend
+var mkdir = denodeify(temp.mkdir)
+var readFile = denodeify(fs.readFile)
+var readdir = denodeify(fs.readdir)
+var path = require('path')
+var now = require('performance-now')
+var ProgressBar = require('progress')
+var getFolderSize = denodeify(require('get-folder-size'))
+var prettierBytes = require('prettier-bytes')
+var prettyMs = require('pretty-ms')
+var tablify = require('tablify').tablify
+var sum = require('math-sum')
+temp.track()
+
+var oldCache
+
+function cleanup () {
+  if (oldCache) {
+    return exec(shellEscape(['npm', 'config', 'set', 'cache', oldCache]))
+  }
+}
+
+function getDeps () {
+  return readFile('package.json', 'utf8').then(function (str) {
+    var json = JSON.parse(str)
+    var deps = extend({}, json.dependencies, json.devDependencies, json.optionalDependencies)
+    console.log('Analyzing ' + Object.keys(deps).length + ' dependencies...')
+    return deps
+  }).catch(function () {
+    throw new Error('no package.json found! bailing out')
+  })
+}
+
+function doNpmInstalls (deps) {
+  var promise = Promise.resolve()
+  var bar = new ProgressBar('[:bar] :percent :etas', {
+    total: Object.keys(deps).length,
+    width: 20
+  })
+  var times = []
+  Object.keys(deps).forEach(function (dep) {
+    var version = deps[dep]
+    promise = promise.then(function () {
+      return mkdir(dep)
+    }).then(function (dir) {
+      return Promise.all([
+        exec(shellEscape([ 'npm', 'config', 'set', 'cache', path.join(dir, '.cache') ])),
+      ]).then(function () {
+        var start = now()
+        return exec(shellEscape([ 'npm', 'install', dep + '@' + version ]), {
+          cwd: dir,
+          env: process.env
+        }).then(function () {
+          var totalTime = now() - start
+          return getFolderSize(path.join(dir, 'node_modules')).then(function (size) {
+            return readdir(path.join(dir, 'node_modules')).then(function (subDeps) {
+              times.push({
+                time: totalTime,
+                size: size,
+                dep: dep,
+                subDeps: subDeps.length - 1
+              })
+              bar.tick()
+            })
+          })
+        })
+      })
+    })
+  })
+  return promise.then(function () {
+    return report(times)
+  })
+}
+
+function report (times) {
+  times = times.sort(function (a, b) {
+    return b.time - a.time
+  })
+  var header = ['Dependency', 'Time', 'Size', '# Deps']
+  var table = [header].concat(times.map(function (time) {
+    return [
+      time.dep,
+      prettyMs(time.time),
+      prettierBytes(time.size),
+      time.subDeps
+    ]
+  }))
+  console.log(tablify(table, {
+    show_index: false,
+    has_header: true
+  }))
+  console.log('Total time: ' + prettyMs(sum(times.map(function (time) {
+    return time.time
+  }))))
+  console.log('Total size: ' + prettierBytes(sum(times.map(function (time) {
+    return time.size
+  }))))
+}
+
+Promise.resolve().then(function () {
+  // remember the user's original `cache` so we can reset it
+  return exec('npm config get cache').then(function (cache) {
+    oldCache = cache.stdout.replace(/\n$/, '')
+  })
+}).then(function () {
+  return getDeps()
+}).then(function (deps) {
+  return doNpmInstalls(deps)
+}).then(function () {
+  return cleanup()
+}).then(function () {
+  process.exit(0)
+}).catch(function (err) {
+  return cleanup().then(function () {
+    console.error(err)
+    console.error(err.stack)
+    process.exit(1)
+  })
+})
+
+process.on('SIGINT', cleanup)
